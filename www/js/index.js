c1588c9ac43aabc9dd5bf8ebd22745d2ca753958
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-04-13 01:55:36 +0200

    Front End for header and add resume section.

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,37 @@
 // JS For the index page. 
 
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+
 /**
  *  Constructor for the resume object
  * @param {int} id
  * @param {string} name
  * @param {string} surname
  * @param {string} goal
- * @param {string[]} attented
+ * @param {string[]} attended
  * @param {string[]} languages
  * @param {string[]} skills
  * @param {string[]} itSkills
@@ -14,12 +39,12 @@
  */
 
 
-function Resume(id, name, surname, goal, attented, languages, skills, itSkills) {                  
+function Resume(id, name, surname, goal, attended, languages, skills, itSkills) {                  
     this.ownId = id;
     this.name = name;
     this.surname = surname;
     this.goal = goal;
-    this.attented = attented;
+    this.attended = attended;
     this.languages = languages;
     this.skills = skills;
     this.itSkills = itSkills;
@@ -31,7 +56,7 @@ function Resume(id, name, surname, goal, attented, languages, skills, itSkills) 
             +    "<name>" + this.name + "</name>"
             +    "<surname>" + this.surname + "</surname>"
             +    "<goal>" + this.goal + "</goal>"
-            +    "<attented>" + this.attented + "</attented>"
+            +    "<attended>" + this.attended + "</attended>"
             +    "<languages>" + this.languages + "</languages>"
             +    "<skills>" + this.skills + "</skills>"
             +    "<itSkills>" + this.itSkills + "</itSkills>"
@@ -53,11 +78,11 @@ function getResumeFromForm() {
     name = $("#textinput-name").val(),
     surname = $("#textinput-surname").val(),
     goal = $("#textinput-goal").val(),
-    attented = $("#textinput-attented").val(),
+    attended = $("#textinput-attended").val(),
     languages = $("#textinput-languages").val(),
     skills = $("#textinput-skills").val(),
     itSkills = $("#textinput-itskills").val();
-    var res = new Resume(ownId, name, surname, goal, attented, languages, skills,
+    var res = new Resume(ownId, name, surname, goal, attended, languages, skills,
         skills, itSkills);
     return res;
 }
@@ -69,11 +94,11 @@ function parseXMLtoHTML(XMLFile) {
         name = $(this).find("name").text(); 
         surname = $(this).find("surname").text(); 
         goal = $(this).find("goal").text(); 
-        attented = $(this).find("attented").text(); 
+        attended = $(this).find("attended").text(); 
         languages = $(this).find("languages").text(); 
         skills = $(this).find("skills").text(); 
         itSkills = $(this).find("itSkills").text();
-        var resume = new Resume(ownId, name, surname, goal, attented, languages,
+        var resume = new Resume(ownId, name, surname, goal, attended, languages,
             skills, itSkills);
         res += resume.toHtml();
     }); 
@@ -81,71 +106,103 @@ function parseXMLtoHTML(XMLFile) {
 }
 
 function getResumes() {
+    // All HTML5 Rocks properties support CORS.
     var resumes;
-    jQuery.ajax({
-        type: "GET",
-        url: "http://resumemanagerrestserver.juanwolf.cloudbees.net/?callback=?",
-        contentType: "text/plain; charset=utf-8 ",
-        dataType: "xml",
-        success: function (data, status, jqXHR) {
-            console.log("[getResumes] xml received : " + data);
-            resumes = data;
-        },
-        error: function (jqXHR, status) {
-            // error handler
+    var url = 'http://resumemanagerrestserver.juanwolf.cloudbees.net/?callback=?';
+
+    var xhr = createCORSRequest('GET', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            // Do something
         }
-    });
+    };
+    xhr.setRequestHeader("Content-type", "text/xml");
+    xhr.setRequestHeader("Connection", "close");
+    if (!xhr) {
+      alert('CORS not supported');
+      return;
+    }
+
+    // Response handlers.
+    xhr.onload = function() {
+      var resumes = xhr.responseXML;
+      alert('Response from CORS request to ' + url + ': ' + resumes);
+    };
+
+    xhr.onerror = function() {
+      $('#errorPopupLink').get(0).click();
+    };
+
+    xhr.send();
     return resumes;
 }
 
+
 function getResumeById(id) {
     var resume;
-    jQuery.ajax({
-        type: "GET",
-        url: "http://resumemanagerrestserver.juanwolf.cloudbees.net/" + id,
-        contentType: "application/xml",
-        dataType: "xml",
-        success: function (data, status, jqXHR) {
-            resume = data;
-        },
-        error: function (jqXHR, status) {
-            // error handler
+    var url = 'http://resumemanagerrestserver.juanwolf.cloudbees.net/'+ id 
+            + '?callback=?';
+
+    var xhr = createCORSRequest('GET', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState==4) {
+            //alert("It worked!");
         }
-    });
+    };
+    xhr.setRequestHeader("Content-type", "text/xml");
+    xhr.setRequestHeader("Connection", "close");
+    if (!xhr) {
+      alert('CORS not supported');
+      return;
+    }
+    // Response handlers.
+    xhr.onload = function() {
+      var resumes = xhr.responseXML;
+    };
+
+    xhr.onerror = function() {
+      $('#errorPopupLink').get(0).click();
+    };
+
+    xhr.send();
     return resume; 
 }
 
 function putResume() {
     var resume = getResumeFromForm();
     var xml = resume.toXml();
-    jQuery.ajax({
-        type: "PUT",
-        url: "http://resumemanagerrestserver.juanwolf.cloudbees.net/?callback=?",
-        contentType: "application/xml",
-        data: xml,
-        dataType: "xml",
-        success: function (data, status, jqXHR) {
-            if (status === 200) {
-                // Send a notif to the user
-            }
-        },
-        error: function (jqXHR, status) {
-           // Send notif not possible.
     
-        }    
-    });    
+    var url = 'http://resumemanagerrestserver.juanwolf.cloudbees.net/?callback=?';
+
+    var xhr = createCORSRequest('PUT', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState==4) {
+            // Sending stuff..
+        }
+    };
+    xhr.setRequestHeader("Content-type", "application/xml");
+    xhr.setRequestHeader("Connection", "close");
+    if (!xhr) {
+      alert('CORS not supported');
+      return;
+    }
+    // Response handlers.
+    xhr.onload = function() {
+      alert("Resume send");
+    };
+
+    xhr.onerror = function() {
+      $('#errorPopupLink').get(0).click();
+    };
+    xhr.send(xml);
 }
 
 
 $(document).ready(function() {
-
     var xmlResumes = getResumes();
     var htmlResumes = parseXMLtoHTML(xmlResumes);
     $("#resumes").append(htmlResumes);
     
-    var xmlResume = getResumeById(1);
-    var htmlResume = parseXMLtoHTML(xmlResume);
-    $("#resumes").append(htmlResume);
     
     $("#add-button").click(function() {
         putResume();
